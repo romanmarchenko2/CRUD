17ea5a8facad81cfc2c4703aadaf0f6111447156
romanmarchenko22 <pumacore22@gmail.com>
2020-04-06 19:05:06 +0300

    RestController added

--- src/main/java/com/marchenko/student/controller/StudentController.java
+++ src/main/java/com/marchenko/student/controller/StudentController.java
@@ -3,17 +3,19 @@ package com.marchenko.student.controller;
 import com.marchenko.student.model.Student;
 import com.marchenko.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class StudentController
 {
@@ -21,39 +23,43 @@ public class StudentController
     StudentService service;
 
     @RequestMapping
-    public String getAllStudents( Model model)
+    public  ResponseEntity<List<Student>> getAllStudents( Model model)
     {
         List<Student> list = service.getAllStudents();
-        model.addAttribute("students", list);
-        return "list-students";
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @RequestMapping(path = {"/edit", "/edit/{id}"})
-    public String editStudentById(Model model,@PathVariable("id") Optional<Long> id)
-    {
+    public ResponseEntity<?>editStudentById(Model model,@PathVariable("id") Optional<Long> id)
+    {Student student;
         if (id.isPresent()) {
-            Student student = service.getStudentsById(id.get());
-            model.addAttribute("student", student);
+            student = service.getStudentsById(id.get());
         } else {
-            model.addAttribute("student", new Student());
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        return "add-edit-student";
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/delete/{id}")
-    public String deleteStudentById(Model model, @PathVariable("id") Long id)
+    public ResponseEntity<?> deleteStudentById(Model model, @PathVariable("id") Long id)
     {
         service.deleteStudentById(id);
-        return "redirect:/";
+        return new ResponseEntity<Student>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(path = "/createStudent", method = RequestMethod.POST)
-    public String createOrUpdateEmployee(@Valid Student student, BindingResult bindingResult)
-    {  if (bindingResult.hasErrors()) {
-        return "add-edit-student";
+    public ResponseEntity<?> createOrUpdateEmployee(@RequestBody @Valid Student student, BindingResult bindingResult, UriComponentsBuilder ucBuilder)
+    {  if (bindingResult.hasErrors())
+    {
+        return new ResponseEntity(HttpStatus.CONFLICT);
     }
-    service.createOrUpdateEmployee(student);
 
-        return "redirect:/";
+    service.createOrUpdateEmployee(student);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("student/{id}").buildAndExpand(student.getId()).toUri());
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
     }
 }
